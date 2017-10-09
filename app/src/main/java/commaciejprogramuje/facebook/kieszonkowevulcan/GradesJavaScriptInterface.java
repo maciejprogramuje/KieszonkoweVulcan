44cfd64bb52760d78758b8b1676c2051cd95543e
maciejprogramuje <maciejsz1979@gmail.com>
2017-10-09 21:41:04 +0200

    ok

--- app/src/main/java/commaciejprogramuje/facebook/kieszonkowevulcan/GradesJavaScriptInterface.java
+++ app/src/main/java/commaciejprogramuje/facebook/kieszonkowevulcan/GradesJavaScriptInterface.java
@@ -21,71 +21,48 @@ public class GradesJavaScriptInterface {
     @JavascriptInterface
     @SuppressWarnings("unused")
     public void processHTML(String html) {
-        System.out.println("=================================================================================");
-        System.out.println("Język polski: " + getGrades(html, "Język polski"));
-        System.out.println("Język angielski: " + getGrades(html, "Język angielski"));
-        System.out.println("Język niemiecki: " + getGrades(html, "Język niemiecki"));
-        System.out.println("Muzyka: " + getGrades(html, "Muzyka"));
-        System.out.println("Historia: " + getGrades(html, "Historia"));
-        System.out.println("Wiedza o społeczeństwie: " + getGrades(html, "Wiedza o społeczeństwie"));
-        System.out.println("Geografia: " + getGrades(html, "Geografia"));
-        System.out.println("Biologia: " + getGrades(html, "Biologia"));
-        System.out.println("Chemia: " + getGrades(html, "Chemia"));
-        System.out.println("Fizyka: " + getGrades(html, "Fizyka"));
-        System.out.println("Matematyka: " + getGrades(html, "Matematyka"));
-        System.out.println("Informatyka: " + getGrades(html, "Informatyka"));
-        System.out.println("Wychowanie fizyczne: " + getGrades(html, ""));
-        System.out.println("Edukacja dla bezpieczeństwa: " + getGrades(html, "Edukacja dla bezpieczeństwa"));
-        System.out.println("Zajęcia techniczne: " + getGrades(html, "Zajęcia techniczne"));
-        System.out.println("Wychowanie do życia w rodzinie: " + getGrades(html, "Wychowanie do życia w rodzinie"));
-        System.out.println("Etyka: " + getGrades(html, "Etyka"));
-        System.out.println("Zajęcia z wychowawcą: " + getGrades(html, "Zajęcia z wychowawcą"));
+        for(int i = 0; i < mainActivity.getSubjects().size(); i++) {
+            set(html, i);
+        }
 
         System.out.println("=================================================================================");
-        System.out.println("Język polski (średnia): " + getAverageGrades(html, "Język polski"));
-        System.out.println("Język angielski (średnia): " + getAverageGrades(html, "Język angielski"));
-        System.out.println("Język niemiecki (średnia): " + getAverageGrades(html, "Język niemiecki"));
-        System.out.println("Muzyka (średnia): " + getAverageGrades(html, "Muzyka"));
-        System.out.println("Historia (średnia): " + getAverageGrades(html, "Historia"));
-        System.out.println("Wiedza o społeczeństwie (średnia): " + getAverageGrades(html, "Wiedza o społeczeństwie"));
-        System.out.println("Geografia (średnia): " + getAverageGrades(html, "Geografia"));
-        System.out.println("Biologia (średnia): " + getAverageGrades(html, "Biologia"));
-        System.out.println("Chemia (średnia): " + getAverageGrades(html, "Chemia"));
-        System.out.println("Fizyka (średnia): " + getAverageGrades(html, "Fizyka"));
-        System.out.println("Matematyka (średnia): " + getAverageGrades(html, "Matematyka"));
-        System.out.println("Informatyka (średnia): " + getAverageGrades(html, "Informatyka"));
-        System.out.println("Wychowanie fizyczne (średnia): " + getAverageGrades(html, "Wychowanie fizyczne"));
-        System.out.println("Edukacja dla bezpieczeństwa (średnia): " + getAverageGrades(html, "Edukacja dla bezpieczeństwa"));
-        System.out.println("Zajęcia techniczne (średnia): " + getAverageGrades(html, "Zajęcia techniczne"));
-        System.out.println("Wychowanie do życia w rodzinie (średnia): " + getAverageGrades(html, "Wychowanie do życia w rodzinie"));
-        System.out.println("Etyka" + getAverageGrades(html, "Etyka"));
-        System.out.println("Zajęcia z wychowawcą (średnia): " + getAverageGrades(html, "Zajęcia z wychowawcą"));
+        for(int i = 0; i < mainActivity.getSubjects().size(); i++) {
+            System.out.println(mainActivity.getSubjects().getName(i) + ": " + mainActivity.getSubjects().getGrades(i) + ", avg=" + mainActivity.getSubjects().getAverage(i));
+        }
         System.out.println("=================================================================================");
     }
 
-    private List<String> getGrades(String html, String subject) {
-        List<String> gradesArray = new ArrayList<>();
+    private void set(String htmlAsString, int subjectIndex) {
+        // ustal nazwę przedmiotu
+        String tempName = mainActivity.getSubjects().getName(subjectIndex);;
+        // wypełnij subjects danymi
+        mainActivity.getSubjects().setGrades(subjectIndex, getGrades(htmlAsString, tempName));
+        mainActivity.getSubjects().setAverage(subjectIndex, getAverageGrades(htmlAsString, tempName));
+    }
+
+    private String getGrades(String html, String subject) {
+        StringBuilder gradesStringBuilder = new StringBuilder("");
         String temp = html.substring(html.indexOf(subject));
         temp = temp.substring(0, temp.indexOf("</tr>"));
         Matcher m = Pattern.compile("[1-6]{1}</span>").matcher(temp);
         while (m.find()) {
-            gradesArray.add(m.group().substring(0, 1));
+            gradesStringBuilder.append(m.group().substring(0, 1)).append(" ");
         }
-        return gradesArray;
+        return gradesStringBuilder.toString();
     }
 
-    private List<String> getAverageGrades(String html, String subject) {
-        List<String> averageGradesArray = new ArrayList<>();
+    private String getAverageGrades(String html, String subject) {
+        StringBuilder averageGradesStringBuilder = new StringBuilder("");
         String temp = html.substring(html.indexOf(subject));
         temp = temp.substring(0, temp.indexOf("</tr>"));
         if (temp.contains("</span></td>")) {
             Matcher m = Pattern.compile("[1-6]{1}[,]?[0-9]?[0-9]?</td>").matcher(temp);
             while (m.find()) {
-                averageGradesArray.add(m.group().replace("</td>", ""));
+                averageGradesStringBuilder.append(m.group().replace("</td>", "")).append(" ");
             }
         } else {
-            averageGradesArray.add("-");
+            averageGradesStringBuilder.append("-");
         }
-        return averageGradesArray;
+        return averageGradesStringBuilder.toString();
     }
 }
--- app/src/main/java/commaciejprogramuje/facebook/kieszonkowevulcan/Subjects.java
+++ app/src/main/java/commaciejprogramuje/facebook/kieszonkowevulcan/Subjects.java
@@ -48,22 +48,22 @@ public class Subjects implements Serializable {
     private List<Subject> subjects = new ArrayList<>();
 
     Subjects() {
-        subjects.add(new Subject("Język polski"));
-        subjects.add(new Subject("Język angielski"));
-        subjects.add(new Subject("Język niemiecki"));
-        subjects.add(new Subject("Muzyka"));
-        subjects.add(new Subject("Historia"));
-        subjects.add(new Subject("Wiedza o społeczeństwie"));
-        subjects.add(new Subject("Geografia"));
-        subjects.add(new Subject("Biologia"));
-        subjects.add(new Subject("Chemia"));
-        subjects.add(new Subject("Fizyka"));
-        subjects.add(new Subject("Matematyka"));
-        subjects.add(new Subject("Informatyka"));
-        subjects.add(new Subject("Edukacja dla bezpieczeństwa"));
-        subjects.add(new Subject("Zajęcia techniczne"));
-        subjects.add(new Subject("Wychowanie do życia w rodzinie"));
-        subjects.add(new Subject("Etyka"));
+        subjects.add(new Subject("Język polski")); // 0
+        subjects.add(new Subject("Język angielski")); // 1
+        subjects.add(new Subject("Język niemiecki")); // 2
+        subjects.add(new Subject("Muzyka")); // 3
+        subjects.add(new Subject("Historia")); // 4
+        subjects.add(new Subject("Wiedza o społeczeństwie")); // 5
+        subjects.add(new Subject("Geografia")); // 6
+        subjects.add(new Subject("Biologia")); // 7
+        subjects.add(new Subject("Chemia")); // 8
+        subjects.add(new Subject("Fizyka")); // 9
+        subjects.add(new Subject("Matematyka")); // 10
+        subjects.add(new Subject("Informatyka")); // 11
+        subjects.add(new Subject("Edukacja dla bezpieczeństwa")); // 12
+        subjects.add(new Subject("Zajęcia techniczne")); // 13
+        subjects.add(new Subject("Wychowanie do życia w rodzinie")); // 14
+        subjects.add(new Subject("Etyka")); // 15
     }
 
     public String getName(int index) {
@@ -84,5 +84,9 @@ public class Subjects implements Serializable {
 
     public void setAverage(int index, String averageString) {
         subjects.get(index).setSubjectAverage(averageString);
+    }
+
+    public int size() {
+        return subjects.size();
     }
 }
